648b403cc22134ba13b9cd5b1188adc0bedc3d38
Kevin T <kevin.tai.mail@gmail.com>
2018-02-16 11:59:29 -0800

    kitchen delegate

--- Kitchen.h
+++ Kitchen.h
@@ -9,6 +9,19 @@
 #import <Foundation/Foundation.h>
 #import "Pizza.h"
 
+@class Kitchen;
+
+@protocol KitchenDelegate
+
+- (BOOL) kitchen : (Kitchen *) kitchen shouldMakePizzaOfSize: (PizzaSize) size andToppings: (NSArray *)toppings;
+- (BOOL) kitchenShouldUpgradeOrder : (Kitchen *) kitchen;
+
+@optional
+
+- (void) kitchenDidMakePizza : (Pizza *) pizza;
+
+@end
+
 @interface Kitchen : NSObject
 
 - (Pizza *)makePizzaWithSize:(PizzaSize)size toppings:(NSArray *)toppings;
